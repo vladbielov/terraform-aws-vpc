6bebaf64d9cd03f3d0f6559172e4a00f2c476459
EC2 Default User <ec2-user@ip-172-31-47-245.eu-west-1.compute.internal>
2020-08-21 20:56:34 +0000

    add aws-vpc

---
+++ ngw.tf
@@ -0,0 +1,11 @@
+# resource "aws_nat_gateway" "gw" {
+#   allocation_id = "${aws_eip.nat.id}"
+#   subnet_id     = "${aws_subnet.example.id}"
+#   tags                        =   "${var.tags}"
+
+# }
+
+# resource "aws_eip" "nat" {
+#   vpc      = true
+#   tags                        =   "${var.tags}"
+# }
